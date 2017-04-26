d80730669a80092498a31adac26a24f544c4fc38
orestinva <or3zt.rvh@gmail.com>
2017-04-26 13:50:50 +0300

    added test for task 330

---
+++ tests/Task330Test.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.junit.*;
+import taskPack.Task330;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by inva on 4/26/2017.
+ */
+public class Task330Test {
+    public Task330 task330 = new Task330();
+    @Test
+    public void testInput(){
+        task330.setInput("555");
+        task330.countResult();
+        assertEquals(task330.getResult(), "6 28 496");
+    }
+
+    @Test
+    public void testException(){
+        task330.setInput("onetwo");
+        task330.countResult();
+        assertEquals(task330.getResult(), "Wrong Input!");
+    }
+
+}
