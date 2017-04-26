707de8663a8e7c5aa840cea7953b3bf801dad969
orestinva <or3zt.rvh@gmail.com>
2017-04-26 22:00:35 +0300

    added tests 330 and 178

--- tests/Task178gTest.java
+++ tests/Task178gTest.java
@@ -1,3 +1,6 @@
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import taskPack.Task178g;
 
@@ -7,7 +10,12 @@ import static org.junit.Assert.assertEquals;
  * Created by inva on 4/26/2017.
  */
 public class Task178gTest {
-    Task178g task178g = new Task178g();
+    private static Task178g task178g;
+
+    @BeforeClass
+    public void setUp(){
+        task178g = new Task178g();
+    }
 
     @Test
     public void testZero(){
@@ -21,5 +29,16 @@ public class Task178gTest {
         task178g.setInput("onetwo");
         task178g.countResult();
         assertEquals(task178g.getResult(), "Please, enter natural number in Integer range.");
+    }
+
+    @After
+    public void afterTest() throws Exception {
+        task178g.setInput("");
+        task178g.setResult("");
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        task178g = null;
     }
 }
--- tests/Task330Test.java
+++ tests/Task330Test.java
@@ -7,7 +7,13 @@ import static org.junit.Assert.*;
  * Created by inva on 4/26/2017.
  */
 public class Task330Test {
-    public Task330 task330 = new Task330();
+    public static Task330 task330;
+
+    @BeforeClass
+    public void setUp(){
+        task330 = new Task330();
+    }
+
     @Test
     public void testInput(){
         task330.setInput("555");
@@ -20,6 +26,17 @@ public class Task330Test {
         task330.setInput("onetwo");
         task330.countResult();
         assertEquals(task330.getResult(), "Wrong Input!");
+    }
+
+    @After
+    public void afterTest() throws Exception {
+        task330.setInput("");
+        task330.setResult("");
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        task330 = null;
     }
 
 }
